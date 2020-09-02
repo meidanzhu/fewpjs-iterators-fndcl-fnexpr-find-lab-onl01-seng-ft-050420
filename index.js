d916d392424b1a3813cbbaa7c9744731229b8b19
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-09-02 05:12:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(record) {
+  let year = record.find((rec) => {return rec.result === 'W'})
+  if (year) return year.year
+}
